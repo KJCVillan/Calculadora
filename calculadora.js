23ba5dd3997f593a4f6355f108f090fdff2d8f65
JOHAN VILLANUEVA <johancondevillan@gmail.com>
2021-04-18 15:07:59 -0500

    Creacion de archivos para cargue a github

---
+++ calculadora.js
@@ -0,0 +1,41 @@
+function setResult(value) 
+{
+  document.getElementById('result').innerHTML = value;
+}
+
+function getResult() 
+{
+  return(document.getElementById('result').innerHTML);
+}
+
+function add(key) 
+{ 
+  var result = getResult();
+  if (result!='0' || isNaN(key)) setResult(result + key);
+  else setResult(key);
+}
+
+function calc() 
+{
+  var result = eval(getResult()); 
+  setResult(result);
+}
+
+function del() 
+{ 
+  setResult(0);
+}
+
+function Finalizar() {        
+
+  var ventana = window.self;
+  ventana.opener = window.self;
+  ventana.close();
+
+}
+
+function Finalizar() {        
+
+  parent.window.close();
+
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+### Features
+
+- Lenguaje: JavaScript
+- Maquetado: HTML + Bootstrap + CSS
+- IDE: Visual Studio Code
+- Convención: Lower camel case
+
+# Calculadora Basica 
+
++ Calculadora basica con 4 operaciones
++ Opcion de salida
